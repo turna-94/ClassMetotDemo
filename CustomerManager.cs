df06c9342c9c45dc374f2a378cf7b923a7c3fab9
Ugur <77512251+turna-94@users.noreply.github.com>
2021-01-18 14:13:50 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankCustomerTracking
+{
+    class CustomerManager
+    {
+        public void List(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id+"-"+customer.FirstName+" "+customer.LastName+"-"+customer.City+"-"+customer.Balance);
+            }
+        }
+
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("New customer added : "+customer.Id + "-" + customer.FirstName + " " + customer.LastName + "-" + customer.City + "-" + customer.Balance);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted : "+customer.Id+"-"+customer.FirstName+" "+customer.LastName);
+        }
+    }
+}
